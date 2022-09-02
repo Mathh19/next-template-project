473f22174b2a867b493bbe3996ea756e8ff3d28e
Matheus <mf635097@gmail.com>
2022-09-02 06:12:01 -0300

    Example project next

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+export default {
+  url: 'https://strapi-react-landing-pages.herokuapp.com/api/pages/?populate[menu][populate]=*&populate[sections][populate]=*',
+  siteName: 'Matheus',
+  defaultSlug: 'landing-page',
+};
